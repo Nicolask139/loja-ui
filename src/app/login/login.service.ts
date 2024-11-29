33e14edc375b20684a6daf5fbd5c2139022e142f
NicolasKIY <128650186+Nicolask139@users.noreply.github.com>
2024-11-28 22:26:44 -0300

    Comeco arquitetura loja

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -2,7 +2,6 @@ import { MessageService } from 'primeng/api';
 import { Injectable } from '@angular/core';
 import { environment } from '../../enviroments/enviroment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,19 +15,15 @@ export class LoginService {
     const body = { email, senha };
   
     this.http.post<{ token: string }>(this.apiUrl, body).subscribe({
-      next: (response) => {
-        // Salva o token no localStorage
+      next: (response) => {  
         localStorage.setItem('authToken', response.token);
-  
-        // Exibe mensagem de sucesso
         this.messageService.add({
           severity: 'success',
           summary: 'Sucesso',
           detail: 'Login realizado com sucesso.',
         });
       },
-      error: (error) => {
-        // Exibe mensagem de erro
+      error: (error) => {   
         this.messageService.add({
           severity: 'error',
           summary: 'Erro',
