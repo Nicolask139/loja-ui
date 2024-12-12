1474c62ce30413332d06e623cc2a274dc82351c3
NicolasKIY <128650186+Nicolask139@users.noreply.github.com>
2024-12-11 22:38:29 -0300

    Refatoração da pagina do produto

--- src/app/produto/produto.component.ts
+++ src/app/produto/produto.component.ts
@@ -1,53 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { ImageModule } from 'primeng/image';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AccordionModule } from 'primeng/accordion';
-import { MenuItem } from 'primeng/api';
-import { MenuModule } from 'primeng/menu';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
 import { CommonModule } from '@angular/common';
-import { PanelModule } from 'primeng/panel';
-import { SplitterModule } from 'primeng/splitter';   
-import { DividerModule } from 'primeng/divider';
+
 
 
 @Component({
   selector: 'app-produto',
   standalone: true,
   imports: [
-    DividerModule,
     CommonModule, 
-    InputTextModule, 
-    MenuModule,
-    ButtonModule, 
-    AccordionModule, 
-    ImageModule, 
     RouterOutlet,
-    PanelModule,
-    SplitterModule
   ],
   templateUrl: './produto.component.html',
   styleUrl: './produto.component.css'
 })
-export class ProdutoComponent implements OnInit{
-  items: MenuItem[] | undefined;
+export class ProdutoComponent{
 
-  ngOnInit(): void {
-    this.items = [
-      {
-          label: 'Menu',
-          items: [
-              {
-                  label: 'Finalizar cadastro!',
-                  icon: 'pi pi-exclamation-circle'
-              },
-              {
-                  label: 'Perfil',
-                  icon: 'pi pi-user'
-              }
-          ]
-        }
-      ]
-  }
 }
+
