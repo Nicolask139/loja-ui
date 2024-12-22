fe5d63038a4f4378daaae46cb070fec0a4efe277
NicolasKIY <128650186+Nicolask139@users.noreply.github.com>
2024-12-22 02:43:41 -0300

    Preparação para entrega do produto minimamente viavel ao cliente

--- src/app/destaque/destaque.component.ts
+++ src/app/destaque/destaque.component.ts
@@ -26,11 +26,57 @@ export class DestaqueComponent implements OnInit{
   artesanatos: any;
   decoracoes: any;
   showBackToTop = false;
+  isMenuVisibleVelas = false;
+  isMenuVisibleDecoracoes = false;
+  isMenuVisibleAromatizadores = false;
 
   constructor(
     private DestaqueService: DestaqueService,
     private router: Router
   ){}
+
+  toggleMenuVelas(event: MouseEvent): void {
+    event.preventDefault(); 
+    this.isMenuVisibleVelas = !this.isMenuVisibleVelas; 
+  }
+  toggleMenuAromatizadores(event: MouseEvent): void {
+    event.preventDefault(); 
+    this.isMenuVisibleAromatizadores = !this.isMenuVisibleAromatizadores; 
+  }
+  toggleMenuDecoracoes(event: MouseEvent): void {
+    event.preventDefault(); 
+    this.isMenuVisibleDecoracoes = !this.isMenuVisibleDecoracoes; 
+  }
+
+  closeMenuVelas(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const menu = document.querySelector('.dropdownVelas');
+    const link = document.querySelector('.menuVelas');
+  
+    if (menu && !menu.contains(target) && target !== link) {
+      this.isMenuVisibleVelas = false; 
+    }
+  }
+
+  closeMenuAromatizadores(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const menu = document.querySelector('.dropdownAroma');
+    const link = document.querySelector('.menuAromatizadores');
+  
+    if (menu && !menu.contains(target) && target !== link) {
+      this.isMenuVisibleAromatizadores = false; 
+    }
+  }
+
+  closeMenuDecoracoes(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const menu = document.querySelector('.dropdownDeco');
+    const link = document.querySelector('.menuDecoracoes');
+  
+    if (menu && !menu.contains(target) && target !== link) {
+      this.isMenuVisibleDecoracoes = false; 
+    }
+  }
 
   ngOnInit(): void {
 
